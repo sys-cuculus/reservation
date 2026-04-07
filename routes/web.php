2acd1343948606ee115ef0d6f38ce087b8f59860
cuculus <cuculus3012@gmail.com>
2026-04-07 11:29:06 +0200

    install laravel breeze

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,20 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ReservationController;
-use App\Http\Controllers\RestaurantController;
-// Route::get('/', function () {
-//     return view('welcome');
-// });
 
-Route::get('/restaurants', [RestaurantController::class, 'index'])->name('restaurants.index');
+Route::get('/', function () {
+    return redirect('login');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
